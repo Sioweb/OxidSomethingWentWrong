cef37aacc9bedac6e477c795644a5c70bd1b3807
Sascha Weidner <=>
2019-05-23 12:03:25 +0200

    Fix sql

--- Core/ShopControl.php
+++ Core/ShopControl.php
@@ -3,37 +3,46 @@
 namespace Sioweb\Oxid\SomethingWentWrong\Core;
 
 use OxidEsales\Eshop\Core\Registry;
-use OxidEsales\Eshop\Core\Session;
 
 class ShopControl extends ShopControl_parent
 {
     public function __construct()
     {
-        $session = oxNew(Session::class);
-        $session->setForceNewSession();
+        Registry::getSession()->setForceNewSession();
         parent::__construct();
     }
 
-    public function start($controllerKey = null, $function = null, $parameters = null, $viewsChain = null)
-    {
-        parent::start($controllerKey, $function, $parameters, $viewsChain);
+    // public function start($controllerKey = null, $function = null, $parameters = null, $viewsChain = null)
+    // {
+    //     parent::start($controllerKey, $function, $parameters, $viewsChain);
+        
+    //     $FeedbackTrace = Registry::getSession()->getVariable("swwFeedbackTrace");
+    //     echo "<pre>" . print_r(Registry::getSession()->getVariable("swwFeedbackTrace"), true) . "</pre>";
 
-        $FeedbackTrace = Registry::getSession()->getVariable("swwFeedbackTrace");
+    //     if(empty($FeedbackTrace)) {
+    //         $FeedbackTrace = [];
+    //     }
 
-        if(empty($FeedbackTrace)) {
-            $FeedbackTrace = [];
-        }
+    //     if(count($FeedbackTrace) === 10) {
+    //         array_shift($FeedbackTrace);
+    //     }
 
-        if(count($FeedbackTrace) === 10) {
-            array_shift($FeedbackTrace);
-        }
+    //     $FeedbackTrace[time()] = [
+    //         'HTTP_HOST' => $_SERVER['HTTP_HOST'],
+    //         'REQUEST_URI' => $_SERVER['REQUEST_URI'],
+    //         'QUERY_STRING' => $_SERVER['QUERY_STRING']
+    //     ];
 
-        $FeedbackTrace[time()] = [
-            'HTTP_HOST' => $_SERVER['HTTP_HOST'],
-            'REQUEST_URI' => $_SERVER['REQUEST_URI'],
-            'QUERY_STRING' => $_SERVER['QUERY_STRING']
-        ];
+    //     Registry::getSession()->setVariable("swwFeedbackTrace", $FeedbackTrace);
+    // }
+    
+public function start($controllerKey = null, $function = null, $parameters = null, $viewsChain = null)
+{
+    parent::start($controllerKey, $function, $parameters, $viewsChain);
+    
+    echo "<pre>" . print_r(Registry::getSession()->getVariable("swwFeedbackTrace"), true) . "</pre>";
 
-        Registry::getSession()->setVariable("swwFeedbackTrace", $FeedbackTrace);
-    }
+    Registry::getSession()->setVariable("swwFeedbackTrace", [1,2,3]);
+}
+
 }
